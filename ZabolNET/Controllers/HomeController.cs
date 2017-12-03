573b746637daa601df3dd52341257880ce446f8c
Akihito759 <GregCudok@yahoo.co.uk>
2017-12-03 23:45:38 +0100

    Stworzenie projektu

--- ZabolNET/Controllers/HomeController.cs
+++ ZabolNET/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ZabolNET.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your apdfhhdfhdfdhf ion page.";
 
             return View();
         }
