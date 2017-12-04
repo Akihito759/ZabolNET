e502d199ffd4bd47c1b6e3ebf6de3fa96f585065
dawidb1 <33998058+dawidb1@users.noreply.github.com>
2017-12-04 21:14:00 +0100

    dodanoApi

--- ZabolNET/Controllers/APIController.cs
+++ ZabolNET/Controllers/APIController.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,41 +12,59 @@ namespace ZabolNET.Controllers
     public class APIController : Controller
     {
         private ZabolNETContext db = new ZabolNETContext();
-
-        // GET: Api
-        public String GetFaculties()
+        public class ApiController : Controller
         {
-            // var x = db.Faculties.Include(f => f.);
-            var x = db.Faculties.Include(s => s.Courses).ToList();
+            // GET: Api
+            private ZabolNETContext db = new ZabolNETContext();
 
-           
-            var json = JsonConvert.SerializeObject(x);
-            return json ;
+            //Course _course = new Course();
+            // GET: Faculties
+            public String GetFacultyJs()
+            {
+                var faculties = db.Faculties.Select(x => x.FacultyName).ToList();
+                return JsonConvert.SerializeObject(faculties);
+            }
+            public String GetCoursesJs(string facultyName)
+            {
+                var id = db.Faculties.Where(f => f.FacultyName == facultyName).Select(f => f.FacultyID).First();
+                var courses = db.Courses.Where(f => f.FacultyID == id).Select(f => f.CourseName).ToList();
+                return JsonConvert.SerializeObject(courses);
+            }
+
+            public String GetYearsJson(string courseName, string facultyName)
+            {
+                var years = GetYears(courseName, facultyName);
+                return JsonConvert.SerializeObject(years);
+            }
+            public List<Year> GetYears(string courseName, string facultyName)
+            {
+                var facCourses = db.Faculties.Include(x => x.Courses);
+                var courses = facCourses.Where(f => f.FacultyName == facultyName).Select(x => x.Courses).First().ToList();
+                var years = courses.Where(x => x.CourseName == courseName).Select(x => x.Years)
+                    .First().ToList();
+
+                return years;
+            }
+
+            public String GetGroupsJson(int year, string courseName, string facultyName)
+            {
+                var years = GetYears(courseName, facultyName);
+                var groups = years.First(x => x.StartYear == year).Groups;
+                return JsonConvert.SerializeObject(groups);
+            }
+            public List<Group> GetGroups(int year, string courseName, string facultyName)
+            {
+                var years = GetYears(courseName, facultyName);
+                var groups = years.First(x => x.StartYear == year).Groups;
+                return groups;
+            }
+
+            public String GetRecordsJs(string groupName, int year, string courseName, string facultyName)
+            {
+                var groups = GetGroups(year, courseName, facultyName);
+                var records = groups.First(x => x.GroupName == groupName);
+                return JsonConvert.SerializeObject(records);
+            }
         }
-
-        public String GetCourses( )
-        {
-            var course = db.Courses.ToList().Where(x=>x.Faculty.FacultyName==faculty);
-            var json = JsonConvert.SerializeObject(course);
-            return json;
-        }
-
-        public String GetYears()
-        {
-            var years = db.Years.ToList();
-            var json = JsonConvert.SerializeObject(years);
-            return json;
-        }
-
-        public String GetGroups()
-        {
-            var groups = db.Groups.ToList();
-            var json = JsonConvert.SerializeObject(groups);
-            return json;
-        }
-
-  
-
-
     }
 }
