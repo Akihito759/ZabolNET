1b1145dc24576759d30e3d1d759ff2f8c3bec087
dawidb1 <33998058+dawidb1@users.noreply.github.com>
2017-12-04 13:09:59 +0100

    Modele

---
+++ ZabolNET/Models/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZabolNET.Models
+{
+    public class Subject
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SubjectID { get; set;}
+        public Year Year { get; set; }
+        public string SubjectName { get; set; }
+    }
+}
